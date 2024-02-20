985f9242229ce9a7700aba90013366b81d2b77c4
Timotraque <157532049+Timotraque@users.noreply.github.com>
2024-02-20 14:43:37 -0500

    Ajout d'integration rectangle

--- Exercice1.py
+++ Exercice1.py
@@ -11,5 +11,26 @@ def solve_integration_numpy(p1=0, p2=0, p3=0, p4=0, debut=0, fin=10**10, n=100):
     integration = np.trapz(y,x)
     return integration, timeit.default_timer() - start
 
-resultat = solve_integration_numpy(1,1,0,6,0,100,1000)
-print(f"Résultat de l'intégrale : {resultat[0]}\ntemps = {resultat[1]}")
+
+def solve_integration_square(p1=0, p2=0, p3=0, p4=0, debut=0, fin=1, n=100):
+    start = timeit.default_timer()
+    pas = (fin - debut)/n
+    integration = 0
+
+    x = []
+    x.append(debut)
+    for i in range(1, n+1):
+        x.append(x[i-1] + pas)
+
+    y = []
+    for i in range(0, n+1):
+        y.append(p1 + p2 * x[i] + p3 * x[i]**2 + p4 * x[i]**3)
+        integration += pas * y[i]
+
+    return integration, timeit.default_timer() - start
+
+resultat_np = solve_integration_numpy(0,1,0,0,0,100,1000)
+print(f"Résultat de l'intégrale : {resultat_np[0]}\ntemps = {resultat_np[1]}")
+
+resultat_sqr = solve_integration_square(0,1,0,0,0,100,10)
+print(f"Résultat de l'intégrale : {resultat_sqr[0]}\ntemps = {resultat_sqr[1]}")
