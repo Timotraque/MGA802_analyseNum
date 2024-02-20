186b9acfa143d806c1793be36bf1b8da1215679b
Timotraque <157532049+Timotraque@users.noreply.github.com>
2024-02-20 15:29:41 -0500

    Ajout de creer_grille()

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # Fichier principal du projet B
+import numpy as np
 
+
+def creer_grille(longueur, largeur, n_x, n_y):
+    x = np.linspace(0, longueur, n_x)
+    y = np.linspace(0, largeur, n_y)
+    xx, yy = np.meshgrid(x,y)
+
+    return xx,yy
+
+
+creer_grille(1,1,100,100)
