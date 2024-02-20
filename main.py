ba590701f49b000b9752f4c88ed594e2597bc22d
Timotraque <157532049+Timotraque@users.noreply.github.com>
2024-02-20 17:20:31 -0500

    Update affichage
    Fonctionnel

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ def creer_grille(x, y, n_x, n_y):
 
     return xx,yy
 
+
 def calcul_intial(x0, y0, xx, yy, A, sigma):
+    delta_t = np.ndarray
     delta_t = A * np.exp(-(np.power(xx - x0, 2)/(2 * np.power(sigma,2)) + np.power(yy - y0, 2)/(2 * np.power(sigma, 2))))
-#    import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()
     return delta_t
+
 
 def afficher_grid(longueur, largeur, delta_t):
     h = plt.contourf(longueur, largeur, delta_t)
@@ -21,13 +24,13 @@ def afficher_grid(longueur, largeur, delta_t):
     plt.colorbar()
     plt.show()
 
-longueur = 1
-largeur = 1
+longueur = 10
+largeur = 10
 n_x = 100
 n_y = 100
 x = np.linspace(0, longueur, n_x)
 y = np.linspace(0, largeur, n_y)
 
 xx, yy = creer_grille(longueur, largeur, n_x, n_y)
-temp = calcul_intial(50, 50, xx, yy, 200, 0.1)
+temp = calcul_intial(longueur/2, largeur/2, xx, yy, 200, 0.1)
 afficher_grid(x, y, temp)
