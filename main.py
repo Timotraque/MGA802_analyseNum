4ce5d2bace39c37f8339e0ba80083673b0fcad35
Timotraque <157532049+Timotraque@users.noreply.github.com>
2024-02-27 17:13:05 -0500

    Update du menu et commentaires

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def creer_grille(longueur, largeur, delta_x, delta_y):
     x = np.arange(0, longueur, delta_x)
     y = np.arange(0, largeur, delta_y)
@@ -9,22 +10,27 @@ def creer_grille(longueur, largeur, delta_x, delta_y):
 
     return xx,yy
 
+
 def surchauffe_initiale(x0, y0, A, sigma, n_x, n_y):
     delta_temp = np.ndarray(shape=(n_x+1,n_y+1), dtype=float)
     delta_temp = A * np.exp(-(np.power(xx - x0, 2)/(2 * np.power(sigma,2)) +
                               np.power(yy - y0, 2)/(2 * np.power(sigma, 2))))
-
     return delta_temp
 
+
 def afficher_grille(longueur, largeur, temperature,titre):
+    plt.title(titre)
     h = plt.contourf(longueur, largeur, temperature, cmap='hot')
+    plt.xlabel('x [m]')
+    plt.ylabel('y [m]')
     plt.axis('scaled')
     plt.colorbar()
     plt.show()
     plt.pause(3)
     plt.close()
     plt.clf()
-    plt.title(titre)
+
+
 def enregistrer_grille_jpeg(longueur, largeur, temperature, numero):
     h = plt.contourf(longueur, largeur, temperature,cmap='hot')
     plt.axis('scaled')
@@ -53,8 +59,6 @@ def calcul_RHS(RHS, temperature, n_x, n_y, delta_x, delta_y, k, pas):
     # Remplacer les valeur RHS <1 par des zeros
     RHS[RHS < 1] = 0
 
-    # Renvoie une grille contenant tous les RHS
-
     return RHS
 
 
@@ -65,45 +69,16 @@ def calcul_T(grille_RHS, ancien_T, dt, longueur, largeur, delta_x, delta_y):
             t_n1[x, y] = ancien_T[x, y] + dt * grille_RHS[x, y]
             if t_n1[x, y] < 1:
                 t_n1[x, y] = 0
-
     return t_n1
+
+
 """____________________________________________
 MAIN
 
 _______________________________________________"""
 
-# [m²/s] Diffusivité thermique pour l'Aluminium
-k = 98.8 * 10**(-6)
-
-# Temperature iniriale hor zone point chaud
-T_zero = 300
-
-# temps total de simulation [s]
-temps_simulation = 600
-
-longueur = 1    #[m]
-largeur = 1     #[m]
-sigma = 0.05
-n_x=80
-n_y=80
-x0=0.5
-y0=0.5
-# [k] Amplitude de la surchauffe au centre"""
-amplitude = 400
-
-#facteur de relaxation de fourrier
-f0 = 0.25
-pas = 0
-delta_x = longueur / (n_x+1)
-delta_y = largeur / (n_y+1)
-dt = min((f0 * np.power(delta_x,2) / k ), (f0 * np.power(delta_y,2) / k))
 
 
-
-x = np.arange(0, longueur,delta_x)
-y = np.arange(0, longueur,delta_y)
-
-xx,yy = creer_grille(longueur, largeur,delta_x,delta_y)
 
 print("Visualisation de la répartition de chaleur\nLe matériau utilisé est l'aluminium !")
 
@@ -116,6 +91,7 @@ amplitude = float(input("Temperature [K] : "))
 sigma = float(input("Ecartement sigma : "))
 x0 = float(input("Position longitudinale du point chaud [m] : "))
 y0 = float(input("Position laterale du point chaud [m] : "))
+T_zero = float(input("Température initiale [k] : "))
 
 print("\nDéfinissez les paramètres de simulation :")
 n_x = int(input("Nombre de points en x : "))
@@ -123,24 +99,40 @@ n_y = int(input("Nombre de points en y : "))
 temps_simulation = float(input("Temps de simulation [s] : "))
 temps_affiche = float(input("Temps de l'affichage [s] : "))
 
+delta_x = longueur / (n_x+1)
+delta_y = largeur / (n_y+1)
+
+x = np.arange(0, longueur,delta_x)
+y = np.arange(0, longueur,delta_y)
+
+#facteur de relaxation de fourrier
+f0 = 0.25
+
+# [m²/s] Diffusivité thermique pour l'Aluminium
+k = 98.8 * 10**(-6)
+
+dt = min((f0 * np.power(delta_x,2) / k ), (f0 * np.power(delta_y,2) / k))
+xx, yy = creer_grille(longueur, largeur,delta_x,delta_y)
+
+# Calcul de la temperature initiale à la surchauffe
 surchauffe = surchauffe_initiale(x0, y0, amplitude, sigma, n_x, n_y)
+
+# Calcul du RHS
 RHS = np.zeros(shape=(n_x+1, n_y+1,int(temps_simulation/dt)), dtype=float)
-temperature = np.ndarray(shape=(n_x+1, n_y+1,int(temps_simulation/dt)), dtype=float)
+
+# Création de la grille de la plaque et addition de la surchauffe
+temperature = np.ndarray(shape=(n_x+1, n_y+1, int(temps_simulation/dt)), dtype=float)
 temperature[:,:,0] = surchauffe + T_zero
-afficher_grille(x, y, temperature[:,:,0])
 
-pas= 0
-# temps apres lequel afficher état de la plaque
-temps_affiche = 120
+# Affichage de la plaque
+afficher_grille(x, y, temperature[:,:,0],"Temperature initiale")
 
 
-
-
-
-while pas*dt< temps_affiche:
+pas = 0
+while pas*dt < temps_affiche:
 
     RHS = calcul_RHS(RHS,temperature, n_x, n_y,delta_x,delta_y, k,pas)
-    temperature[:,:,pas+1] = temperature[:,:,pas] + dt*RHS[:,:,0]
+    temperature[:, :, pas+1] = temperature[:,:,pas] + dt*RHS[:,:,0]
 
     #import pdb; pdb.set_trace()
     pas +=1
